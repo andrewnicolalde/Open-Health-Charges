37fce2869f1d2aa8ff90eba0e30d88a88b5c16e5
Zack Clark-Kington <admin@pr0xysystems.co.uk>
2018-01-13 16:59:32 +0000

    Adds app.js, allows parsing of csv into json

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+function convertCSVtoJSON(csv){
+	let lines = csv.split(/\r\n|\n/);
+	var data = [];
+
+	for(var i = 0; i < lines.length; i++){
+		data.push(lines[i].split(','));
+	}
+
+	return data;
+}
