9b420c4dfd82185498debfa316f948ed218f2d31
Andrew Nicolalde <andrew.nicolalde@gmail.com>
2018-01-13 18:26:33 +0000

    Add map

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,5 +10,9 @@ function convertCSVtoJSON(csv){
 }
 
 window.onload = function(){
-	fetch('/')
+	var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+}).addTo(mymap);
 }
