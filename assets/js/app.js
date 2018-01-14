aa17868ee1fe7adfa2356963cf9c59bcf409a311
Marcus Messer <marcus.messer.2016@live.rhul.ac.uk>
2018-01-14 06:49:55 +0000

    Pan shit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,14 @@
+var mymap = null;
 window.onload = function(){
-	var mymap = L.map('map').setView([51.505, -0.09], 13);
+	mymap = L.map('map').setView([51.505, -0.09], 13);
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     	maxZoom: 18,
 	}).addTo(mymap);
 
 	getHeatMap().addTo(mymap);
+};
+
+function pan(){
+	mymap.panBy([leap.getMovementDirection() * 100, leap.getMovementDirection() * 100]);
 }
