264a95949a70f5f103b6918ce0d015417b6b34c1
Zack Clark-Kington <admin@pr0xysystems.co.uk>
2018-01-14 03:28:02 +0000

    Fixes Andrew

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,9 @@
-window.addEventListener("load", () => {
-	var map = new ol.Map({
-	    target: 'map',
-	    layers: [
-	        new ol.layer.Tile({
-	            source: new ol.source.OSM()
-	        })
-	    ],
-	    view: new ol.View({
-	        center: ol.proj.fromLonLat([37.41, 8.82]),
-	        zoom: 4
-	    })
-	});
-})
+window.onload = function(){
+	var mymap = L.map('map').setView([51.505, -0.09], 13);
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    	maxZoom: 18,
+	}).addTo(mymap);
+
+	getHeatMap().addTo(mymap);
+}
