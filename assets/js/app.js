166285590a391e3d7b01f4a8cbf567767cf58802
Andrew Nicolalde <andrew.nicolalde@gmail.com>
2018-01-14 07:44:02 +0000

    Fix zoom, show USA by default

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,11 @@
 var mymap = null;
 window.onload = function(){
-	mymap = L.map('map').setView([51.505, -0.09], 13);
+	mymap = L.map('map').setView([38.0003554, -99.9364875], 13);
 	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     	maxZoom: 18,
 	}).addTo(mymap);
-
+mymap.setZoom(5);
 	getHeatMap().addTo(mymap);
 };
 
