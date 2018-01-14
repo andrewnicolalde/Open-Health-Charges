a750d553e136019d0ccaf220df986ff99d81187e
Marcus Messer <marcus.messer.2016@live.rhul.ac.uk>
2018-01-14 07:44:14 +0000

    FUFCK

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,6 +5,7 @@ window.onload = function(){
     	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     	maxZoom: 18,
 	}).addTo(mymap);
+	pan();
 
 	//getHeatMap().addTo(mymap);
 };
